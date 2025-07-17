e4534e479be6c8f32cbaba3e1fa27bddcd2b397c
Viktor Andreev <viktor.andreev@onlyoffice.com>
2025-07-17 17:14:12 +0600

    Add worksheets conversion

--- OOXML/XlsxFormat/Worksheets/Worksheet.cpp
+++ OOXML/XlsxFormat/Worksheets/Worksheet.cpp
@@ -51,6 +51,7 @@
 
 #include "../../Binary/XlsbFormat/FileTypes_SpreadsheetBin.h"
 #include "../../../MsBinaryFile/XlsFile/Format/Binary/CFStreamCacheWriter.h"
+#include "../../../MsBinaryFile/XlsFile/Format/Logic/WorksheetSubstream.h"
 
 #include "../../../OdfFile/Common/logging.h"
 
@@ -360,6 +361,13 @@ namespace OOX
 				return workSheetStream;
 			}
 
+		}
+		XLS::BaseObjectPtr CWorksheet::toXLS() const
+		{
+			auto worksheetPtr = new XLS::WorksheetSubstream(0);
+			auto sheetPtr = XLS::BaseObjectPtr(worksheetPtr);
+
+			return sheetPtr;
 		}
         void CWorksheet::WriteBin(XLS::StreamCacheWriterPtr& writer) const
         {
