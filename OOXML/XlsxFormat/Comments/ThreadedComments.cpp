cb217be2a2f29676679d825aa92df7bb7806cc42
Elena Subbotina <Elena.Subbotina@onlyoffice.com>
2026-04-30 10:38:36 +0300

    .

--- OOXML/XlsxFormat/Comments/ThreadedComments.cpp
+++ OOXML/XlsxFormat/Comments/ThreadedComments.cpp
@@ -58,7 +58,7 @@ namespace OOX
 		{
 			writer.WriteString(L"<person");
 				WritingStringNullableAttrEncodeXmlString(L"displayName", displayName, *displayName);
-				WritingStringNullableAttrString(L"id", id, *id);
+				WritingStringNullableAttrString(L"id", id, id->ToString());
 				WritingStringNullableAttrEncodeXmlString(L"userId", userId, *userId);
 				WritingStringNullableAttrEncodeXmlString(L"providerId", providerId, *providerId);
 			writer.WriteString(L"/>");
